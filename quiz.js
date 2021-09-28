48c3423f8d3cd42e8885760a5299722c7efb0cf9
darshan <darshandamre5@gmail.com>
2021-09-28 17:39:59 +0530

    bug fix

--- quiz.js
+++ quiz.js
@@ -4,12 +4,30 @@ const output = document.querySelector("#output");
 
 const answers = ["90", "right angled", "3"];
 
+const check = () => {
+  let radios = document.getElementsByName(
+    "question1" || "question2" || "question3"
+  );
+
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      return true;
+    }
+  }
+  return false;
+};
+
 const calculateScore = () => {
   let score = 0;
   let index = 0;
   let data = new FormData(quizForm);
+
+  if (!check()) {
+    return (output.innerText = "Please select options");
+  }
+
   for (let value of data.values()) {
-    console.log(value);
+    console.log(`-->${value}<--`);
     if (answers.includes(value)) {
       score += 1;
     }
